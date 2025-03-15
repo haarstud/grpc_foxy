53851dbca1e4be05a791efa803d374d99bba99e5
Yoav Kleinberger, PhD <haarcuba@gmail.com>
2025-03-15 20:52:41 +0200

    regenerated route guide

--- route_guide/route_guide_pb2.pyi
+++ route_guide/route_guide_pb2.pyi
@@ -4,16 +4,8 @@ from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Opti
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class Feature(_message.Message):
-    __slots__ = ["location", "name"]
-    LOCATION_FIELD_NUMBER: _ClassVar[int]
-    NAME_FIELD_NUMBER: _ClassVar[int]
-    location: Point
-    name: str
-    def __init__(self, name: _Optional[str] = ..., location: _Optional[_Union[Point, _Mapping]] = ...) -> None: ...
-
 class Point(_message.Message):
-    __slots__ = ["latitude", "longitude"]
+    __slots__ = ("latitude", "longitude")
     LATITUDE_FIELD_NUMBER: _ClassVar[int]
     LONGITUDE_FIELD_NUMBER: _ClassVar[int]
     latitude: int
@@ -21,15 +13,23 @@ class Point(_message.Message):
     def __init__(self, latitude: _Optional[int] = ..., longitude: _Optional[int] = ...) -> None: ...
 
 class Rectangle(_message.Message):
-    __slots__ = ["hi", "lo"]
-    HI_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("lo", "hi")
     LO_FIELD_NUMBER: _ClassVar[int]
-    hi: Point
+    HI_FIELD_NUMBER: _ClassVar[int]
     lo: Point
+    hi: Point
     def __init__(self, lo: _Optional[_Union[Point, _Mapping]] = ..., hi: _Optional[_Union[Point, _Mapping]] = ...) -> None: ...
 
+class Feature(_message.Message):
+    __slots__ = ("name", "location")
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    LOCATION_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    location: Point
+    def __init__(self, name: _Optional[str] = ..., location: _Optional[_Union[Point, _Mapping]] = ...) -> None: ...
+
 class RouteNote(_message.Message):
-    __slots__ = ["location", "message"]
+    __slots__ = ("location", "message")
     LOCATION_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
     location: Point
@@ -37,13 +37,13 @@ class RouteNote(_message.Message):
     def __init__(self, location: _Optional[_Union[Point, _Mapping]] = ..., message: _Optional[str] = ...) -> None: ...
 
 class RouteSummary(_message.Message):
-    __slots__ = ["distance", "elapsed_time", "feature_count", "point_count"]
+    __slots__ = ("point_count", "feature_count", "distance", "elapsed_time")
+    POINT_COUNT_FIELD_NUMBER: _ClassVar[int]
+    FEATURE_COUNT_FIELD_NUMBER: _ClassVar[int]
     DISTANCE_FIELD_NUMBER: _ClassVar[int]
     ELAPSED_TIME_FIELD_NUMBER: _ClassVar[int]
-    FEATURE_COUNT_FIELD_NUMBER: _ClassVar[int]
-    POINT_COUNT_FIELD_NUMBER: _ClassVar[int]
+    point_count: int
+    feature_count: int
     distance: int
     elapsed_time: int
-    feature_count: int
-    point_count: int
     def __init__(self, point_count: _Optional[int] = ..., feature_count: _Optional[int] = ..., distance: _Optional[int] = ..., elapsed_time: _Optional[int] = ...) -> None: ...
