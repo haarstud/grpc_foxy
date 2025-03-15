ac61515092a17b03a71938d8d2a1cfcc5025a3ab
Yoav Kleinberger, PhD <haarcuba@gmail.com>
2025-03-16 00:05:36 +0200

    basic cli

--- source/foxy_grpc/cli.py
+++ source/foxy_grpc/cli.py
@@ -1,3 +1,20 @@
-def main() -> int:
-    print("hi, this is foxy_grpc")
-    return 0
+import click
+
+
+@click.group()
+@click.pass_context
+def main(context):
+    context.ensure_object(dict)
+
+
+@main.command()
+@click.pass_context
+@click.option("--port", default=3333, type=int, help="Port to listen on")
+def server():
+    pass
+
+@main.command()
+@click.pass_context
+@click.argument("server_url", type=str)
+def client(server_url):
+    pass
